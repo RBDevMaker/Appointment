bf810ae554f27d0a474baef14943f507782dcc3c
RBDevMaker <rachelle.briscoe@navontech.com>
2025-12-09 21:27:39 -0500

    Add admin credentials display next to scissor icon for demo purposes

--- appointments/urls.py
+++ appointments/urls.py
@@ -6,15 +6,26 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("service/<int:service_id>/", views.index, name="index-service"),
+    path("services/<str:service_ids>/", views.index, name="index-services"),
     path(
         "service/<int:service_id>/hairdresser/<int:hairdresser_id>",
         views.index,
         name="index-hairdresser",
     ),
     path(
+        "services/<str:service_ids>/hairdresser/<int:hairdresser_id>",
+        views.index,
+        name="index-services-hairdresser",
+    ),
+    path(
         "service/<int:service_id>/hairdresser/<int:hairdresser_id>/date/<str:date_string>",
         views.index,
         name="index-date",
+    ),
+    path(
+        "services/<str:service_ids>/hairdresser/<int:hairdresser_id>/date/<str:date_string>",
+        views.index,
+        name="index-services-date",
     ),
     path("create", views.create, name="create"),
     path("cancel/<str:token>/", views.cancel, name="cancel"),
